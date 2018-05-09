bf627dc835f75ccacfcd24eeb0eb8871bdcbb862
msheriey <sherieymourad1997@gmail.com>
2018-05-10 00:58:33 +0200

    Selenium Login Initial Tests

---
+++ tst/selenium/tests/login/TestLoginAsAdmin.java
@@ -0,0 +1,21 @@
+package selenium.tests.login;;
+
+import selenium.Lib;
+import selenium.tests.WebDriverTestCase;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestLoginAsAdmin extends WebDriverTestCase {
+
+    @Test
+    public void loginAsAdmin() {
+
+        userAction.login("Abdo", "Temsah");
+        userAction.clickHome();
+
+        Assert.assertEquals(driver.getCurrentUrl(), Lib.Pages.ADMIN);
+
+    }
+
+}
---
+++ tst/selenium/tests/login/TestLoginSQLInject.java
@@ -0,0 +1,21 @@
+package selenium.tests.login;
+
+import selenium.Lib;
+import selenium.tests.WebDriverTestCase;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class TestLoginSQLInject extends WebDriverTestCase {
+
+    @Test
+    public void loginSQLInject() {
+
+        userAction.login("Nayra", "' or ' 1=1");
+
+        Assert.assertEquals(driver.getCurrentUrl(), Lib.Pages.LOGIN);
+
+    }
+
+}
