3fde9411a75254941d58c2c444226c8b5776bc39
msheriey <sherieymourad1997@gmail.com>
2018-05-11 20:46:50 +0200

    Added Brand Data Duplication Tests

--- tst/unit/TestBrandServices.java
+++ tst/unit/TestBrandServices.java
@@ -28,62 +28,51 @@ public class TestBrandServices {
 
     @BeforeTest
     public void setup() {
-
         MockitoAnnotations.initMocks(this);
         brandRepository = Mockito.mock(BrandRepository.class);
         brandServices = new BrandServices(brandRepository);
-
+        when(brandRepository.findAll()).thenReturn(brands);
     }
 
     private Object addBrand(Brand brand) {
-
-        when(brandRepository.save(Matchers.any(Brand.class))).thenReturn(brand);
-
         return brandServices.add(brand).getBody();
-
     }
 
     @DataProvider(name = "brandValidDataProvider")
     public Object[][] brandValidDataProvider() {
-
         return new Object[][] { {"Brand_1"}, {"Brand1"}, {"BRAND"} };
-
     }
 
     @Test(dataProvider = "brandValidDataProvider")
     public void addBrandValidData(String name) {
-
         Brand brand = new Brand().setName(name);
-
         Assert.assertEquals(addBrand(brand), true);
 
         brands.add(brand);
+        when(brandRepository.findOne(brand.getName())).thenReturn(brand);
+        when(brandRepository.exists(brand.getName())).thenReturn(true);
+    }
 
+    @Test(dataProvider = "brandValidDataProvider", dependsOnMethods = "addBrandValidData")
+    public void addBrandDuplicateData(String name) {
+        Brand brand = new Brand().setName(name);
+        Assert.assertEquals(addBrand(brand), false);
     }
 
     @DataProvider(name = "brandInvalidDataProvider")
     public Object[][] brandInvalidDataProvider() {
-
         return new Object[][] { {" "}, {"@A!"}, {"or 1=1"} };
-
     }
 
    @Test(dataProvider = "brandInvalidDataProvider")
     public void addBrandInvalidData(String name) {
-
        Brand brand = new Brand().setName(name);
-
        Assert.assertEquals(addBrand(brand), false);
-
     }
 
     @Test(dependsOnMethods = "addBrandValidData")
     public void getAllBrands() {
-
-        when(brandRepository.findAll()).thenReturn(brands);
-
         Assert.assertEquals(brandServices.all().getBody().equals(brands), true);
-
     }
 
 }
