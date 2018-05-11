bb5562f404fbcfcc44e8b05b5843d8188271fb45
msheriey <sherieymourad1997@gmail.com>
2018-05-11 20:58:35 +0200

    Added Category Data Duplication Tests

--- tst/unit/TestCategoryServices.java
+++ tst/unit/TestCategoryServices.java
@@ -28,63 +28,51 @@ public class TestCategoryServices {
 
     @BeforeTest
     public void setup() {
-
         MockitoAnnotations.initMocks(this);
         categoryRepository = Mockito.mock(CategoryRepository.class);
         categoryServices = new CategoryServices(categoryRepository);
-
+        when(categoryRepository.findAll()).thenReturn(categories);
     }
 
     private Object addCategory(Category category) {
-
-        when(categoryRepository.save(Matchers.any(Category.class))).thenReturn(category);
-
         return categoryServices.add(category).getBody();
-
     }
-
 
     @DataProvider(name = "categoryValidDataProvider")
     public Object[][] categoryValidDataProvider() {
-
         return new Object[][] { {"Category_1"}, {"Category1"}, {"CATEGORY"} };
-
     }
 
     @Test(dataProvider = "categoryValidDataProvider")
     public void addCategoryValidData(String name) {
-
         Category category = new Category().setName(name);
-
         Assert.assertEquals(addCategory(category), true);
 
         categories.add(category);
+        when(categoryRepository.findOne(category.getName())).thenReturn(category);
+        when(categoryRepository.exists(category.getName())).thenReturn(true);
+    }
 
+    @Test(dataProvider = "categoryValidDataProvider", dependsOnMethods = "addCategoryValidData")
+    public void addCategoryDuplicateData(String name) {
+        Category category = new Category().setName(name);
+        Assert.assertEquals(addCategory(category), false);
     }
 
     @DataProvider(name = "categoryInvalidDataProvider")
     public Object[][] categoryInvalidDataProvider() {
-
-        return new Object[][] { {" "}, {"@A!"}, {"or 1=1"} };
-
+        return new Object[][] { {" "}, {"@A!<script>alert(\"die\");</script>"}, {"or 1=1"} };
     }
 
     @Test(dataProvider = "categoryInvalidDataProvider")
     public void addCategoryInvalidData(String name) {
-
         Category category = new Category().setName(name);
-
         Assert.assertEquals(addCategory(category), false);
-
     }
 
     @Test(dependsOnMethods = "addCategoryValidData")
     public void getAllCategories() {
-
-        when(categoryRepository.findAll()).thenReturn(categories);
-
         Assert.assertEquals(categoryServices.all().getBody().equals(categories), true);
-
     }
 
 }
